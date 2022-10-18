1245828d6b082a2b8537332f6535bb4cb46614f5
avinashgantaa <avinash.ganta39@gmail.com>
2022-10-18 11:46:33 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,7 @@
+/* variables are used to store data, variables syntax is variable declaration and initialization
+varibles have different keywords like var,let,const
+var is introduced in ES1
+let and const Introduced in Es6
+*/
+var i=1
+console.log(i);
