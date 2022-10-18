412794d3d08857401a69a67b0e1c8acbbb5a2c41
avinashgantaa <avinash.ganta39@gmail.com>
2022-10-18 12:08:35 +0530

    next

--- variables.js
+++ variables.js
@@ -2,6 +2,15 @@
 varibles have different keywords like var,let,const
 var is introduced in ES1
 let and const Introduced in Es6
+if global variable effected due to local variable it is called global pollution
+it is arised in var key word,
+we can solve it by let key word
+if we call variable with out declaring a varible, it should throw error, but in var key word it throws 
+undefined, by using let keyword we can solve this. this is called variable hoisting.
 */
-var i=1
+var i=100;
+{
+    i=200;
+}
 console.log(i);
+
